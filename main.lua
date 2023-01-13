99f9b3074eed6e0e16d5d716383066ec4f2fe056
Ethan Olsen <131eolsen@gmail.com>
2023-01-13 05:57:44 -0800

    chore: add folder glob require and local storage
    add Test object file

--- main.lua
+++ main.lua
@@ -1,5 +1,28 @@
-function love.load()
+Object = require "libs/rxi/classic"
 
+function love.load()
+	local object_files = {}
+	recursiveEnumerate('objects', object_files)
+	requireFiles(object_files)
+end
+
+function recursiveEnumerate(folder, file_list)
+	local items = love.filesystem.getDirectoryItems(folder)
+	for _, item in ipairs(items) do
+			local file = folder .. '/' .. item
+			if love.filesystem.getInfo(file, "file") then
+					table.insert(file_list, file)
+			elseif love.filesystem.getInfo(file, "directory") then
+					recursiveEnumerate(file, file_list)
+			end
+	end
+end
+
+function requireFiles(files)
+	for _, file in ipairs(files) do
+			local file = file:sub(1, -5)
+			require(file)
+	end
 end
 
 function love.update(dt)
@@ -9,3 +32,4 @@ end
 function love.draw()
 
 end
+
---
+++ objects/Test.lua
@@ -0,0 +1,13 @@
+Test = Object:extend()
+
+function Test:new()
+		print("Test:new")
+end
+
+function Test:update(dt)
+		print("Test:update")
+end
+
+function Test:draw()
+		print("Test:draw")
+end
