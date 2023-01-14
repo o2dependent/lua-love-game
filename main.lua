2f5ad2619d0bd09842ad5bcd56449a677835ca7b
Ethan Olsen <131eolsen@gmail.com>
2023-01-14 00:45:37 -0800

    chore: add stage and new main organization

--- main.lua
+++ main.lua
@@ -1,25 +1,8 @@
 Object = require "libs/rxi/classic"
 Input = require "libs/boipushy/Input"
 Timer = require "libs/chrono/Timer"
+requireAllFromFolder = require "utils/requireAllFromFolder"
 
-function recursiveEnumerate(folder, file_list)
-	local items = love.filesystem.getDirectoryItems(folder)
-	for _, item in ipairs(items) do
-			local file = folder .. '/' .. item
-			if love.filesystem.getInfo(file, "file") then
-					table.insert(file_list, file)
-			elseif love.filesystem.getInfo(file, "directory") then
-					recursiveEnumerate(file, file_list)
-			end
-	end
-end
-
-function requireFiles(files)
-	for _, file in ipairs(files) do
-			local file = file:sub(1, -5)
-			require(file)
-	end
-end
 
 local objects = {}
 
@@ -37,9 +20,18 @@ function animate_hp(new_hp)
 end
 
 function love.load()
+	-- require all object
+	requireAllFromFolder("objects")
+
+	-- create input listener and timer
 	input = Input()
 	timer = Timer()
 
+	-- initialize rooms and current room var
+	rooms = {}
+	current_room = nil
+
+	-- health bar init functions
 	input:bind('d', function()
 		if hp > 0 then
 			hp = hp - 10
@@ -54,12 +46,37 @@ function love.load()
 end
 
 function love.update(dt)
+	-- keep timer up to date
 	timer:update(dt)
+
+	-- update room if it exists
+	if current_room then current_room:update(dt) end
 end
 
 function love.draw()
+	-- draw health bar
 	love.graphics.setColor(255, 0, 0, 1)
 	love.graphics.rectangle('fill', hp_rect_1.x - full_hp_width/2, hp_rect_1.y - hp_rect_1.h/2, hp_rect_1.w, hp_rect_1.h)
 	love.graphics.setColor(255, 0, 0, 0.5)
 	love.graphics.rectangle('fill', hp_rect_2.x - full_hp_width/2, hp_rect_2.y - hp_rect_2.h/2, hp_rect_2.w, hp_rect_2.h)
+
+	-- draw room if it exists
+	if current_room then current_room:draw() end
 end
+
+function addRoom(room_type, room_name, ...)
+	local room = _G[room_type](room_name, ...)
+	rooms[room_name] = room
+	return room
+end
+
+function gotoRoom(room_type, room_name, ...)
+	if current_room and rooms[room_name] then
+		if current_room.deactivate then current_room:deactivate() end
+		current_room = rooms[room_name]
+		if current_room.activate then current_room:activate() end
+	else
+		current_room = addRoom(room_type, room_name, ...)
+	end
+end
+
---
+++ objects/Stage.lua
@@ -0,0 +1,13 @@
+Stage = Object:extend()
+
+function Stage:new()
+
+end
+
+function Stage:update(dt)
+
+end
+
+function Stage:draw()
+
+end
