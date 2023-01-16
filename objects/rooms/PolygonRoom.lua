00b8c93a31b29b68afeeae4a7818ca3d2806afd2
Ethan Olsen <131eolsen@gmail.com>
2023-01-16 05:09:33 -0800

    chore: remove testing rooms

--- objects/rooms/PolygonRoom.lua
+++
@@ -1,6 +0,0 @@
-PolygonRoom = Stage:extend()
-
-function PolygonRoom:draw()
-	love.graphics.setColor(255, 255, 255)
-	love.graphics.polygon("fill", 75, 45, 400, 45, 20, 300, 0, 300)
-end
--- objects/rooms/RectangleRoom.lua
+++
@@ -1,7 +0,0 @@
-RectangleRoom = Stage:extend()
-
-function RectangleRoom:draw()
-	love.graphics.setColor(255, 255, 255)
-	love.graphics.rectangle("fill", 200, 150, 400, 300)
-end
-
